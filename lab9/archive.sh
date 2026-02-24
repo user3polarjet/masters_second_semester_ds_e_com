fa49f3a5aaa778f2b3c93b3fc5b85ed65b4dcf1e
Your Name <you@example.com>
2026-02-24 18:18:51 +0200

    wip

--- lab9/archive.sh
+++ lab9/archive.sh
@@ -1,2 +1,2 @@
-typst compile Панченко_Сергій_ІП_51мн_8.typ
-zip -r Панченко_Сергій_ІП_51мн_8.zip Панченко_Сергій_ІП_51мн_8.pdf build main.py
+typst compile Панченко_Сергій_ІП_51мн_9.typ
+zip -r Панченко_Сергій_ІП_51мн_9.zip Панченко_Сергій_ІП_51мн_9.pdf build main.py Fire_Stations.shp
